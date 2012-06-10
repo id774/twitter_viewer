ab2b23df4b554d2c86def3cb0db722d1e8b068f7
id774 <idnanashi@gmail.com>
2012-06-10 20:12:55 +0900

    30 Remove obsolete CI script

--- ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PATH=/opt/ruby/1.9.3/bin:/opt/ruby/1.9.2/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-export RAILS_ENV=test
-bundle exec rake db:migrate:reset
-bundle exec rake test
